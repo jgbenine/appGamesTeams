bd4c27351ea3d963471a9566628e5dc9429de633
jgbenine <joaobenine@gmail.com>
2024-05-26 13:46:33 -0300

    init: header

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import theme from "@theme/index";
 import { ThemeProvider } from "styled-components";
+import { StatusBar } from "react-native";
 import { Groups } from "@screens/Groups";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from "@components/Loading";
@@ -9,6 +10,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />} 
     </ThemeProvider>
   );
